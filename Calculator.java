db0ed7ece2b523497b4af5d31065a4f086a017c2
Tanvi-0009 <tanvi.shelar@cumminscollege.in>
2026-02-10 12:43:22 +0530

    Add calculator main code

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,8 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Calculator Project - Team Version");
+        System.out.println("Team members will add operations using branches.");
+    }
+}
 
